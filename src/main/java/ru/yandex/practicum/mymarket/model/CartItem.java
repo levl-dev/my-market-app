0ec5eccc275b649bbb33267baf24a51ead270ef7
Лебедев Владислав Анатольевич <vl.lebedev@cifra-broker.ru>
2026-04-16 19:53:37 +0300

    migrate models from jpa to r2dbc

--- src/main/java/ru/yandex/practicum/mymarket/model/CartItem.java
+++ src/main/java/ru/yandex/practicum/mymarket/model/CartItem.java
@@ -1,33 +1,23 @@
 package ru.yandex.practicum.mymarket.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.relational.core.mapping.Column;
 
-@Entity
-@Table(name = "cart_items")
+@Table("cart_items")
 @Getter
 @Setter
 @NoArgsConstructor
 public class CartItem {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "item_id", nullable = false)
-    private Item item;
+    @Column("item_id")
+    private Long itemId;
 
-    @Column(nullable = false)
     private int count;
 }
